312b89a241cc7a1b490e7650b1165fba390526d1
Michael Jones <m.pricejones@gmail.com>
2015-06-21 16:48:26 +0100

    Successfully handling a clone request
    That is all it can do but it is pretty cool to see it actuall work.
    Requires manually deleting the repository afterwards unfortunately!
    
    We use the ramda library for some functional approaches to handling the
    code and then break down and do a bunch of stuff in imperative style. I
    still need to work on my functional chops it seems.

--- src/community-bot.js
+++ src/community-bot.js
@@ -3,12 +3,13 @@
 var Q = require('q')
 // var request = Q.denodeify(require('request'))
 var GithubApi = require('github');
+var r = require('ramda');
 
 var config = require('../etc/config');
 
 var github = new GithubApi({
     version: "3.0.0",
-    debug: true,
+    // debug: true,
     protocol: "https",
     host: "api.github.com", // should be api.github.com for GitHub
     timeout: 5000,
@@ -23,7 +24,44 @@ github.authenticate({
     token: config.apiToken,
 });
 
-var issues = Q.denodeify(github.issues.repoIssues);
 
-issues({user: 'homeless-beta', repo: 'contact'}).then(console.log)
+var findCommands = r.compose(
+        r.map(r.compose(r.trim, r.drop(3))),
+        r.filter(r.compose(r.equals('hb>'), r.take(3)))
+        )
 
+var dispatchCommand = (command) => {
+    var split = r.split(' ', command);
+    var name = split[0];
+    var args = split.slice(1);
+
+    if (name !== 'clone') {
+        throw Error('Unrecognised command:', name);
+    }
+
+    var repoRef = r.split('/', args[0]);
+    var repoUser = repoRef[0];
+    var repoName = repoRef[1];
+
+    var fork = Q.denodeify(github.repos.fork);
+
+    console.log('forking', repoUser, '/', repoName);
+
+    fork({ user: repoUser, repo: repoName })
+        .then(console.log)
+        .catch(console.log);
+}
+
+function main() {
+
+    var issues = Q.denodeify(github.issues.repoIssues);
+
+    var processIssue = r.compose(r.map(dispatchCommand), findCommands, r.split('\r\n'), r.prop('body'));
+
+    issues({user: 'homeless-beta', repo: 'contact'})
+        .then(r.map(processIssue))
+        .catch(console.log)
+
+}
+
+main();
